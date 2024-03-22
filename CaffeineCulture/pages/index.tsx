8b35c9cc155eab8eb4fca45e4ac8a62a59885049
Eli Bradford <elibradford227@gmail.com>
2024-03-22 18:09:42 -0500

    updated eslint dependencies, begun typescript conversion

--- CaffeineCulture/pages/index.tsx
+++ CaffeineCulture/pages/index.tsx
@@ -1,16 +1,52 @@
 import { Button } from 'react-bootstrap';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import { useAuth } from '../utils/context/authContext';
 import { getPosts, searchPosts } from '../utils/data/postData';
 import PostCard from '../components/cards/PostCard';
 
+interface PostData {
+  id: number;
+  title: string;
+  content: string;
+  date: string;
+  like_count: number;
+  category: {
+    id: number;
+    name: string;
+  };
+  user: {
+    id: number;
+    username: string;
+    bio: string;
+    join_date: string;
+    uid: string;
+  };
+  comments: CommentData[]; 
+  liked: boolean;
+}
+
+interface CommentData {
+  id: number;
+  user: {
+    id: number;
+    username: string;
+    bio: string;
+    join_date: string;
+    uid: string;
+  };
+  post: PostData;
+  content: string;
+  date: string;
+  parent: CommentData | null;
+}
+
 function Home() {
   const { user } = useAuth();
-  const [posts, setPosts] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [posts, setPosts] = useState<PostData[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>('');
 
-  const getAllPosts = (uid) => {
+  const getAllPosts = (uid: string) => {
     getPosts(uid).then((res) => setPosts(res));
   };
 
@@ -18,28 +54,21 @@ function Home() {
     getAllPosts(user.uid);
   }, [user.uid]);
 
-  const handleChange = (event) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     searchPosts(searchTerm, user.uid).then((res) => setPosts(res));
     setSearchTerm('');
-  };
-
-  const handleKeyPress = (e) => {
-    if (e.key === 'Enter' && !e.shiftKey) {
-      e.preventDefault();
-      handleSubmit(e);
-    }
   };
 
   return (
     <div
       className="justify-content-center align-content-center"
     >
-      <Form onSubmit={handleSubmit} onKeyDown={handleKeyPress} style={{ padding: '10px' }}>
+      <Form onSubmit={handleSubmit}  style={{ padding: '10px' }}>
         <input
           type="text"
           placeholder="Search Posts"
